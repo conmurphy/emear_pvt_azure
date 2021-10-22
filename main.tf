bf977a12c700c3ca840906d3616392d19ad93805
Conor Murphy <conmurph@cisco.com>
2021-10-22 14:05:11 +0200

    Initial

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "azurerm" {
+    features {}
+
+    subscription_id = var.subscription_id
+    client_id       = var.client_id
+    client_secret   = var.client_secret
+    tenant_id       = var.tenant_id
+
+}
+
+resource "azurerm_resource_group" "emear_pvt" {
+  name     = "emear-pvt-rm-health"
+  location = "Germany West Central"
+}
+
+resource "azurerm_redis_cache" "example" {
+  name                = "emear-pvt-rm-health"
+  location            = azurerm_resource_group.emear_pvt.location
+  resource_group_name = azurerm_resource_group.emear_pvt.name
+  capacity            = 0
+  family              = "C"
+  sku_name            = "Basic"
+  enable_non_ssl_port = true
+  minimum_tls_version = "1.2"
+
+  redis_configuration {
+      enable_authentication = true
+  }
+}
